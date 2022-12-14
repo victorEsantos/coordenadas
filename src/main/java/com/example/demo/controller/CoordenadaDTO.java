4840747c68f0ac3a5c7b55900fb551336b26d7db
victorEsantos <victor.eduardo.sntos@gmail.com>
2022-12-14 11:45:20 -0300

    ajuste igor

--- src/main/java/com/example/demo/controller/CoordenadaDTO.java
+++ src/main/java/com/example/demo/controller/CoordenadaDTO.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
+@Data
 public class CoordenadaDTO {
     private Double latitude;
     private Double longitude;
